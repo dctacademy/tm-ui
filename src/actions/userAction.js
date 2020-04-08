6359adb4c321f6b0da3a7e4374a4baeaa3c00aa1
aniruddha <ani@dctacademy.com>
2020-04-08 15:10:35 +0530

    login user & set token

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,5 +1,23 @@
 import axios from '../config/axios'
 
+export const startLoginUser = (formData, redirect) => {
+    return (dispatch) => {
+        axios.post('/users/login', formData)
+            .then((response) => {
+                if(response.data.hasOwnProperty('error')) {
+                    alert(response.data.error)
+                } else {
+                    alert('successfully logged in')
+                    localStorage.setItem('authToken', response.data.token)
+                    redirect()
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+}
+
 export const startRegisterUser = (formData, redirect) => {
     return (dispatch) => {
         axios.post('/users/register', formData)
